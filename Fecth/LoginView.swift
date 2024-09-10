3fdf0ee98e87d40a421af744a0816fe17ea7a048
Gerson Arce <arceger@hotmail.com>
2024-09-10 17:08:43 +0100

    funcional

--- Fecth/LoginView.swift
+++ Fecth/LoginView.swift
@@ -1,3 +1,112 @@
+//import Foundation
+//import SwiftUI
+//
+//struct LoginView: View {
+//    @State private var email = ""
+//    @State private var password = ""
+//    @Binding var isLoggedIn: Bool
+//    @State private var showingAlert = false
+//    @State private var alertMessage = ""
+//    @AppStorage("loggedInUserName") var loggedInUserName: String = ""
+//    @AppStorage("loggedInUserRole") var loggedInUserRole: String = ""
+//    @AppStorage("authToken") var authToken: String = ""
+//    
+//        var body: some View {
+//            NavigationView {
+//                VStack {
+//                    Text("Login")
+//                        .font(.largeTitle)
+//                        .padding()
+//    
+//                    TextField("Email", text: $email)
+//                        .textFieldStyle(RoundedBorderTextFieldStyle())
+//                        .padding()
+//    
+//                    SecureField("Password", text: $password)
+//                        .textFieldStyle(RoundedBorderTextFieldStyle())
+//                        .padding()
+//    
+//                    Button("Login") {
+//                        login()
+//                    }
+//                    .padding()
+//    
+//                    NavigationLink("Register", destination: RegisterView())
+//                        .padding()
+//    
+//                }
+//                .alert(isPresented: $showingAlert) {
+//                    Alert(title: Text("Login"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+//                }
+//            }
+//        }
+//
+//
+//    func login() {
+//        guard let url = URL(string: "https://c238-2001-8a0-7309-4a00-95cb-fa61-647d-31c8.ngrok-free.app/api/src/login.php") else { return }
+//        
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "POST"
+//        
+//        guard !email.isEmpty, !password.isEmpty else {
+//            alertMessage = "Email and password cannot be empty."
+//            showingAlert = true
+//            return
+//        }
+//        
+//        let json: [String: Any] = ["email": email, "password": password]
+//        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+//        
+//        request.httpBody = jsonData
+//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//            if error != nil {
+//                DispatchQueue.main.async {
+//                    self.alertMessage = "Network error. Please try again."
+//                    self.showingAlert = true
+//                }
+//                return
+//            }
+//
+//            if let httpResponse = response as? HTTPURLResponse {
+//                if httpResponse.statusCode == 200 {
+//                    if let data = data, let jsonResponse = try? JSONSerialization.jsonObject(with: data) as? [String: Any] {
+//                        // Captura o nome, papel e token do usuário
+//                        if let name = jsonResponse["name"] as? String,
+//                           let role = jsonResponse["role"] as? String,
+//                           let token = jsonResponse["token"] as? String {
+//                            DispatchQueue.main.async {
+//                                self.loggedInUserName = name
+//                                self.loggedInUserRole = role
+//                                self.authToken = token // Armazenando o token
+//                                self.isLoggedIn = true
+//                            }
+//                        } else {
+//                            DispatchQueue.main.async {
+//                                self.alertMessage = "Invalid response format."
+//                                self.showingAlert = true
+//                            }
+//                        }
+//                    } else {
+//                        DispatchQueue.main.async {
+//                            self.alertMessage = "Unable to parse server response."
+//                            self.showingAlert = true
+//                        }
+//                    }
+//                } else {
+//                    DispatchQueue.main.async {
+//                        self.alertMessage = "Invalid email or password."
+//                        self.showingAlert = true
+//                    }
+//                }
+//            }
+//        }.resume()
+//    }
+//}
+
+
+
 import Foundation
 import SwiftUI
 
@@ -10,46 +119,53 @@ struct LoginView: View {
     @AppStorage("loggedInUserName") var loggedInUserName: String = ""
     @AppStorage("loggedInUserRole") var loggedInUserRole: String = ""
     @AppStorage("authToken") var authToken: String = ""
-    
-        var body: some View {
-            NavigationView {
-                VStack {
-                    Text("Login")
-                        .font(.largeTitle)
-                        .padding()
-    
-                    TextField("Email", text: $email)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding()
-    
-                    SecureField("Password", text: $password)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding()
-    
-                    Button("Login") {
-                        login()
-                    }
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Login")
+                    .font(.largeTitle)
                     .padding()
-    
-                    NavigationLink("Register", destination: RegisterView())
-                        .padding()
-    
+
+                TextField("Email", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+
+                Button("Login") {
+                    login()
                 }
-                .alert(isPresented: $showingAlert) {
-                    Alert(title: Text("Login"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-                }
+                .padding()
+
+                NavigationLink("Register", destination: RegisterView())
+                    .padding()
+            }
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("Login"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
             }
         }
-
+    }
 
     func login() {
-        guard let url = URL(string: "https://c238-2001-8a0-7309-4a00-95cb-fa61-647d-31c8.ngrok-free.app/api/src/login.php") else { return }
+        // Recupera a URL base do Info.plist
+        guard let baseURL = Bundle.main.object(forInfoDictionaryKey: "API_URL") as? String else {
+            alertMessage = "API URL não configurada corretamente."
+            showingAlert = true
+            return
+        }
+        
+        let loginURL = "\(baseURL)login.php"
+        
+        guard let url = URL(string: loginURL) else { return }
         
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         
         guard !email.isEmpty, !password.isEmpty else {
-            alertMessage = "Email and password cannot be empty."
+            alertMessage = "Email e senha não podem estar vazios."
             showingAlert = true
             return
         }
@@ -63,7 +179,7 @@ struct LoginView: View {
         URLSession.shared.dataTask(with: request) { data, response, error in
             if error != nil {
                 DispatchQueue.main.async {
-                    self.alertMessage = "Network error. Please try again."
+                    self.alertMessage = "Erro de rede. Por favor, tente novamente."
                     self.showingAlert = true
                 }
                 return
@@ -84,19 +200,19 @@ struct LoginView: View {
                             }
                         } else {
                             DispatchQueue.main.async {
-                                self.alertMessage = "Invalid response format."
+                                self.alertMessage = "Formato de resposta inválido."
                                 self.showingAlert = true
                             }
                         }
                     } else {
                         DispatchQueue.main.async {
-                            self.alertMessage = "Unable to parse server response."
+                            self.alertMessage = "Não foi possível interpretar a resposta do servidor."
                             self.showingAlert = true
                         }
                     }
                 } else {
                     DispatchQueue.main.async {
-                        self.alertMessage = "Invalid email or password."
+                        self.alertMessage = "Email ou senha inválidos."
                         self.showingAlert = true
                     }
                 }
@@ -104,6 +220,3 @@ struct LoginView: View {
         }.resume()
     }
 }
-
-
-
--- Fecth/RegisterView.swift
+++ Fecth/RegisterView.swift
@@ -1,10 +1,180 @@
+//import SwiftUI
+//
+//struct RegisterView: View {
+//    @State private var email = ""
+//    @State private var confirmEmail = ""
+//    @State private var password = ""
+//    @State private var confirmPassword = ""
+//    @State private var role = "tecnico"
+//    @State private var nome = ""
+//    @State private var tel = ""
+//    @State private var city = ""
+//    @State private var endereco = ""
+//    @State private var showingAlert = false
+//    @State private var alertMessage = ""
+//    @State private var isLoading = false // Estado para controlar o indicador de carregamento
+//
+//    var body: some View {
+//        VStack {
+//            Text("Register")
+//                .font(.largeTitle)
+//                .padding()
+//            
+//            TextField("Nome", text: $nome)
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+//                .padding()
+//
+//            TextField("Telefone", text: $tel)
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+//                .padding()
+//
+//            TextField("Cidade", text: $city)
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+//                .padding()
+//
+//            TextField("Endereço", text: $endereco)
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+//                .padding()
+//
+//            TextField("Email", text: $email)
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+//                .padding()
+//            
+//            TextField("Confirme o Email", text: $confirmEmail) // Campo de confirmação de email
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+//                .padding()
+//
+//            SecureField("Senha", text: $password)
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+//                .padding()
+//            
+//            SecureField("Confirme a Senha", text: $confirmPassword) // Campo de confirmação de senha
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+//                .padding()
+//            
+//            Picker("Role", selection: $role) {
+//                Text("Tecnico").tag("tecnico")
+//                Text("Admin").tag("admin")
+//            }
+//            .pickerStyle(SegmentedPickerStyle())
+//            .padding()
+//            
+//            if isLoading { // Mostra o indicador de carregamento enquanto isLoading for true
+//                ProgressView()
+//                    .padding()
+//            } else {
+//                Button("Register") {
+//                    register()
+//                }
+//                .padding()
+//            }
+//        }
+//        .alert(isPresented: $showingAlert) {
+//            Alert(title: Text("Registration"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+//        }
+//    }
+//    
+//    func register() {
+//        // Validação dos campos
+//        guard !email.isEmpty, !password.isEmpty, !nome.isEmpty, !tel.isEmpty, !city.isEmpty, !endereco.isEmpty else {
+//            alertMessage = "Todos os campos são obrigatórios."
+//            showingAlert = true
+//            return
+//        }
+//        
+//        // Validação de confirmação de email e senha
+//        guard email == confirmEmail else {
+//            alertMessage = "Os emails não coincidem."
+//            showingAlert = true
+//            return
+//        }
+//        
+//        guard password == confirmPassword else {
+//            alertMessage = "As senhas não coincidem."
+//            showingAlert = true
+//            return
+//        }
+//        
+//        isLoading = true // Inicia o carregamento
+//        
+//        guard let baseURL = Bundle.main.object(forInfoDictionaryKey: "API_URL") as? String else {
+//            alertMessage = "API URL não configurada corretamente."
+//            showingAlert = true
+//            return
+//        }
+//
+//        guard let url = URL(string: "\(baseURL)register.php") else { return }
+//        
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "POST"
+//        
+//        let json: [String: Any] = [
+//            "email": email,
+//            "password": password,
+//            "role": role,
+//            "nome": nome,
+//            "tel": tel,
+//            "city": city,
+//            "endereco": endereco
+//        ]
+//        
+//        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+//        request.httpBody = jsonData
+//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//        
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//            DispatchQueue.main.async {
+//                isLoading = false // Finaliza o carregamento
+//
+//                if let error = error {
+//                    print("Error: \(error)")
+//                    self.alertMessage = "Network error. Please try again."
+//                    self.showingAlert = true
+//                    return
+//                }
+//
+//                if let httpResponse = response as? HTTPURLResponse {
+//                    self.alertMessage = "" // Limpa a mensagem anterior
+//                    switch httpResponse.statusCode {
+//                    case 201:
+//                        self.alertMessage = "Registration successful!"
+//                        clearFields() // Limpa os campos após o registro bem-sucedido
+//                    case 400:
+//                        self.alertMessage = "Bad Request. Please check your input."
+//                    case 409:
+//                        self.alertMessage = "Email already registered. Please use a different email."
+//                    case 401:
+//                        self.alertMessage = "Unauthorized. Please check your credentials."
+//                    default:
+//                        self.alertMessage = "Unexpected error. Please try again."
+//                    }
+//                    self.showingAlert = true
+//                }
+//            }
+//        }.resume()
+//    }
+//
+//    // Função para limpar os campos
+//    func clearFields() {
+//        email = ""
+//        confirmEmail = ""
+//        password = ""
+//        confirmPassword = ""
+//        nome = ""
+//        tel = ""
+//        city = ""
+//        endereco = ""
+//        role = "tecnico"
+//    }
+//}
+
 import SwiftUI
 
 struct RegisterView: View {
     @State private var email = ""
-    @State private var confirmEmail = "" // Novo campo de confirmação de email
+    @State private var confirmEmail = ""
     @State private var password = ""
-    @State private var confirmPassword = "" // Novo campo de confirmação de senha
+    @State private var confirmPassword = ""
     @State private var role = "tecnico"
     @State private var nome = ""
     @State private var tel = ""
@@ -12,14 +182,14 @@ struct RegisterView: View {
     @State private var endereco = ""
     @State private var showingAlert = false
     @State private var alertMessage = ""
-    @State private var isLoading = false // Estado para controlar o indicador de carregamento
+    @State private var isLoading = false // Para controlar a ampulheta de carregamento
 
     var body: some View {
         VStack {
             Text("Register")
                 .font(.largeTitle)
                 .padding()
-            
+
             TextField("Nome", text: $nome)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
@@ -40,15 +210,15 @@ struct RegisterView: View {
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
             
-            TextField("Confirme o Email", text: $confirmEmail) // Campo de confirmação de email
+            TextField("Confirme o Email", text: $confirmEmail)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
 
-            SecureField("Senha", text: $password)
+            SecureField("Password", text: $password)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
             
-            SecureField("Confirme a Senha", text: $confirmPassword) // Campo de confirmação de senha
+            SecureField("Confirme o Password", text: $confirmPassword)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
             
@@ -58,9 +228,9 @@ struct RegisterView: View {
             }
             .pickerStyle(SegmentedPickerStyle())
             .padding()
-            
-            if isLoading { // Mostra o indicador de carregamento enquanto isLoading for true
-                ProgressView()
+
+            if isLoading {
+                ProgressView() // Mostra uma ampulheta enquanto carrega
                     .padding()
             } else {
                 Button("Register") {
@@ -73,7 +243,7 @@ struct RegisterView: View {
             Alert(title: Text("Registration"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
         }
     }
-    
+
     func register() {
         // Validação dos campos
         guard !email.isEmpty, !password.isEmpty, !nome.isEmpty, !tel.isEmpty, !city.isEmpty, !endereco.isEmpty else {
@@ -96,8 +266,14 @@ struct RegisterView: View {
         }
         
         isLoading = true // Inicia o carregamento
+        
+        guard let baseURL = Bundle.main.object(forInfoDictionaryKey: "API_URL") as? String else {
+            alertMessage = "API URL não configurada corretamente."
+            showingAlert = true
+            return
+        }
 
-        guard let url = URL(string: "https://c238-2001-8a0-7309-4a00-95cb-fa61-647d-31c8.ngrok-free.app/api/src/register.php") else { return }
+        guard let url = URL(string: "\(baseURL)register.php") else { return }
         
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
@@ -148,8 +324,8 @@ struct RegisterView: View {
         }.resume()
     }
 
-    // Função para limpar os campos
     func clearFields() {
+        // Limpa os campos após o registro bem-sucedido
         email = ""
         confirmEmail = ""
         password = ""
@@ -158,6 +334,5 @@ struct RegisterView: View {
         tel = ""
         city = ""
         endereco = ""
-        role = "tecnico"
     }
 }
